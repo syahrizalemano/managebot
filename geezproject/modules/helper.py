b72bf9ccd151b517398db5fa79db281af0278ce9
Anobots <89107294+syahrizalemano@users.noreply.github.com>
2023-02-25 11:22:45 +0800

    Update helper.py

--- geezproject/modules/helper.py
+++ geezproject/modules/helper.py
@@ -8,7 +8,7 @@ from geezproject.utils import edit_or_reply, geez_cmd
 async def var(event):
     await edit_or_reply(
         event,
-        "**Daftar Lengkap Vars Dari GeezProjects:** [KLIK DISINI](https://telegra.ph/List-Variabel-Heroku-untuk-GeezProjects-09-22)",
+        "**Daftar Lengkap Vars Dari bot:** [KLIK DISINI](https://telegra.ph/List-Variabel-Heroku-untuk-GeezProjects-09-22)",
     )
 
 
@@ -16,11 +16,9 @@ CMD_HELP.update(
     {
         "helper": f"**Plugin : **`helper`\
         \n\n  𝘾𝙤𝙢𝙢𝙖𝙣𝙙 :** `{cmd}ihelp`\
-        \n  ❍▸ : **Bantuan Untuk GeezProjects.\
+        \n  ❍▸ : **Bantuan bot.\
         \n\n  𝘾𝙤𝙢𝙢𝙖𝙣𝙙 :** `{cmd}listvar`\
         \n  ❍▸ : **Melihat Daftar Vars.\
-        \n\n  𝘾𝙤𝙢𝙢𝙖𝙣𝙙 :** `{cmd}repo`\
-        \n  ❍▸ : **Melihat Repository GeezProjects.\
         \n\n  𝘾𝙤𝙢𝙢𝙖𝙣𝙙 :** `{cmd}string`\
         \n  ❍▸ : **Link untuk mengambil String GeezProjects.\
     "
